be56394a1b0a533281b427b1728f3a0ef486a614
alwinruby <alwinsolanky@gmail.com>
2017-03-07 22:00:50 +0000

    First server connection

--- components/App.jsx
+++ components/App.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
 import UserSection from './users/UserSection.jsx';
 import MessageSection from './messages/MessageSection.jsx';
+
 class App extends Component{
   constructor(props){
     super(props);
@@ -9,14 +10,46 @@ class App extends Component{
       channels: [],
       users: [],
       messages: [],
-      activeChannel: {}
+      activeChannel: {},
+      connected: false
     };
+  }
+  componentDidMount(){
+    let ws = this.ws = new WebSocket('ws://echo.websocket.org');
+    ws.onmessage = this.message.bind(this);
+    ws.onopen = this.open.bind(this);
+    ws.close = this.close.bind(this);
+  }
+  message(e){
+    const event = JSON.parse(e.data);
+    if(event.name === 'channel add'){
+      this.newChannel(event.data);
+    }
+  }
+  open(){
+    this.setState({connected: true});
+  }
+  close(){
+    this.setState({connected: false});
+  }
+  newChannel(channel){
+    let {channels} = this.state;
+    channels.push(channel);
+    this.setState({channels});
   }
   addChannel(name){
     let {channels} = this.state;
-    channels.push({id: channels.length, name});
-    this.setState({channels});
+    //channels.push({id: channels.length, name});
+    //this.setState({channels});
     // TODO: Send to server
+    let msg = {
+      name: 'channel add',
+      data: {
+        id: channels.length,
+        name
+      }
+    }
+    this.ws.send(JSON.stringify(msg))
   }
   setChannel(activeChannel){
     this.setState({activeChannel});
